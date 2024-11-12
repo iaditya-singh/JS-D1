7ab864c96e7fb08c57bdb35fbe6c294ea1a4ceb8
iadityasingh-3616 <iadityasingh2468@gmail.com>
2024-11-12 14:29:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+
+// console.log("This is js Class")
+// a = 6
+// console.log(a)
+// console.log(5)
+
+// document.write("this is js")
+
+// variable
+
+// a = 10;
+// b = 20;
+// c = a + b;
+
+// num_one = 10;
+// numOne = 45;
+
+// first_name = "Aditya"
+// lastName="Singh"
+
+// console.log(c);
+
+// declaration ( let, var , const ) (declaration Keyword)
+
+var first_name = "Aditya"
+
+var Last_name = "singh"
+
+// concatenation
+
+console.log(first_name + " " + Last_name )
+
+// int fname = 45
+
+// let
+
+let data = 67
+console.log(data)
+
+//const
+
+const number = 98765432
+console.log(number)
+
+// ( = assignment operator)
+
+// variable is a storage location / address pointer
+ 
+// concatanation
+
+// string h to concate hoga number h to arithmatic operation hoga
+
